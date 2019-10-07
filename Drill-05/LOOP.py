b95b6b453806524dc6e07f382d48de6d88a10e22
Easyless(laptop) <49525970+tirat0001@users.noreply.github.com>
2019-10-07 19:22:09 +0900

    왼클릭 입력받기
    왼클릭 입력받기

--- Drill-05/LOOP.py
+++ Drill-05/LOOP.py
@@ -6,6 +6,7 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global next_x, next_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -14,6 +15,10 @@ def handle_events():
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            next_x = event.x
+            next_y = event.y
+
 
     pass
 
@@ -21,6 +26,7 @@ def handle_events():
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 Cursor = load_image('hand_arrow.png')
+character = load_image('animation_sheet.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
@@ -30,6 +36,7 @@ hide_cursor()
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 0, 100, 100, x, y)
     Cursor.draw(x, y)
     update_canvas()
     frame = (frame + 1) % 8
