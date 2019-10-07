f4c8d9bb3b20bdf34dede1208bb8e0af3c1da506
Easyless(laptop) <49525970+tirat0001@users.noreply.github.com>
2019-10-07 18:44:32 +0900

    ADD resource
    ADD resource

---
+++ venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\abba\Desktop\Study\2-2\2DGP\Drills\2016180016-2DGP-DRILLS\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
---
+++ venv/Scripts/pip3.7-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\abba\Desktop\Study\2-2\2DGP\Drills\2016180016-2DGP-DRILLS\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3.7'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3.7')()
+    )
