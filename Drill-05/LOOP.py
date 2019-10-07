50853628d47429d155f868c40e4485da0220b817
Easyless(laptop) <49525970+tirat0001@users.noreply.github.com>
2019-10-07 19:27:21 +0900

    마우스 분리

--- Drill-05/LOOP.py
+++ Drill-05/LOOP.py
@@ -5,20 +5,20 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x, y
+    global Cursor_x, Cursor_y
+    global Character_x, Character_y
     global next_x, next_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            Cursor_x, Cursor_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
             next_x = event.x
             next_y = event.y
-
 
     pass
 
@@ -29,22 +29,18 @@ Cursor = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+Cursor_x, Cursor_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+Character_x, Character_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    Cursor.draw(x, y)
+    Cursor.draw(Cursor_x, Cursor_y)
     update_canvas()
     frame = (frame + 1) % 8
 
     handle_events()
 
 close_canvas()
-
-
-
-
