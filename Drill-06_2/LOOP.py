d6b168799ea6fd55121a3110206c21cb34b4fc82
Easyless(laptop) <49525970+tirat0001@users.noreply.github.com>
2019-10-09 18:40:27 +0900

    delete something & add func

--- Drill-06_2/LOOP.py
+++ Drill-06_2/LOOP.py
@@ -1,33 +1,19 @@
 from pico2d import *
+import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def handle_events():
-    global running, Move
-    global Cursor_x, Cursor_y
+    global running
     global Character_x, Character_y
-    global next_x, next_y
-    global start_x, start_y
     global Character_Dir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            Cursor_x, Cursor_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            if Character_x <= Cursor_x:
-                Character_Dir = 0
-            elif Character_x >= Cursor_x:
-                Character_Dir = 1
-            start_x = Character_x
-            start_y = Character_y
-            next_x = Cursor_x
-            next_y = Cursor_y
-            Move = True
 
         pass
 
@@ -41,38 +27,48 @@ running = True
 Cursor_x, Cursor_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 Character_x, Character_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
-hide_cursor()
-Character_Dir = 0  # 0 이면 left, 1이면 right
-Move = False
+# hide_cursor()
+Character_Dir = 0  # 0 이면 right, 1이면 left
+x_points = [random.randint(100, 1100) for n in range(10)]
+y_points = [random.randint(100, 900) for n in range(10)]
 
 while running:
 
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    Cursor.draw(Cursor_x, Cursor_y)
 
-    if Move == True:
-        for i in range(0, 100 + 1, 1):
-            clear_canvas()
-            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-            Cursor.draw(Cursor_x, Cursor_y)
-            t = i / 100
-            Character_x = (1 - t) * start_x + t * next_x
-            Character_y = (1 - t) * start_y + t * next_y
-            if Character_Dir == 0:
-                character.clip_draw(frame * 100, 100, 100, 100, Character_x - 20, Character_y + 20)
-            elif Character_Dir == 1:
-                character.clip_draw(frame * 100, 0, 100, 100, Character_x - 20, Character_y + 20)
-            frame = (frame + 1) % 8
-            update_canvas()
-            handle_events()
-        Move = False
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * x_points[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * x_points[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * x_points[2] + (t ** 3 - t ** 2) * x_points[3]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * y_points[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * y_points[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * y_points[2] + (t ** 3 - t ** 2) * y_points[3]) / 2
+        if Character_Dir == 1:
+            character.clip_draw(frame * 100, 100, 100, 100, Character_x, Character_y)
+        elif Character_Dir == 0:
+            character.clip_draw(frame * 100, 0, 100, 100, Character_x, Character_y)
+        frame = (frame + 1) % 8
+        update_canvas()
+        handle_events()
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * x_points[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * x_points[2] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * x_points[3] + (t ** 3 - t ** 2) * x_points[4]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * y_points[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * y_points[2] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * y_points[3] + (t ** 3 - t ** 2) * y_points[4]) / 2
+        if Character_Dir == 1:
+            character.clip_draw(frame * 100, 100, 100, 100, Character_x, Character_y)
+        elif Character_Dir == 0:
+            character.clip_draw(frame * 100, 0, 100, 100, Character_x, Character_y )
+        frame = (frame + 1) % 8
+        update_canvas()
+        handle_events()
 
-    character.clip_draw(frame * 100, (100- Character_Dir*100), 100, 100, Character_x - 20, Character_y + 20)
+    character.clip_draw(frame * 100, (100 - Character_Dir * 100), 100, 100, Character_x - 20, Character_y + 20)
     frame = (frame + 1) % 8
     update_canvas()
     handle_events()
     delay(0.01)
-
 
 close_canvas()
