00b01e021f88dc47be37312c2bd8fd6ad59e0d86
김현진 <kimhj06160@gmail.com>
2019-11-11 23:34:19 +0900

    init

--- Drill-05/LOOP.py
+++ Drill-05/LOOP.py
@@ -4,7 +4,7 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def handle_events():
-    global running, Move
+    global Play, Move
     global Cursor_x, Cursor_y
     global Character_x, Character_y
     global next_x, next_y
@@ -13,11 +13,11 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            Play = False
         elif event.type == SDL_MOUSEMOTION:
             Cursor_x, Cursor_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+            Play = False
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             if Character_x <= Cursor_x:
                 Character_Dir = 0
@@ -37,7 +37,7 @@ kpu_ground = load_image('KPU_GROUND.png')
 Cursor = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
 
-running = True
+Play = True
 Cursor_x, Cursor_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 Character_x, Character_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
@@ -45,7 +45,7 @@ hide_cursor()
 Character_Dir = 0  # 0 이면 left, 1이면 right
 Move = False
 
-while running:
+while Play:
 
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
